e77b1b454537fc7e16f5e0296c84257dba7bdfaa
Felix Richter <felixrichter@felix2016.wireless.mountsinai.org>
2016-06-06 10:28:24 -0400

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'chabedpymprdet',
+    packages = ['bedpymp'],
+    version = '0.0.0',
+    author='Felix Richter',
+    author_email='frichter91@gmail.com',
+    url='',
+    description = 'BedTools wrappers for multi processer scaling'
+)
