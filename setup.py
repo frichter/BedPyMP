0d469ba0c9a3baf776694e521f491082c77e0a46
Felix Richter <felixrichter@felix2016.wireless.mountsinai.org>
2016-06-06 10:39:32 -0400

    updated license and setup

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from distutils.core import setup
 setup(
     name = 'chabedpymprdet',
     packages = ['bedpymp'],
-    version = '0.0.0',
+    version = '0.1',
     author='Felix Richter',
     author_email='frichter91@gmail.com',
-    url='',
-    description = 'BedTools wrappers for multi processer scaling'
+    url='https://github.com/frichter/bedpymp',
+    url='https://github.com/frichter/bedpymp/tarball/0.1',
+    description = 'PyBedTools wrappers for multi processer scaling'
 )
